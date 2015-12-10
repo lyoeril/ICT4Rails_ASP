e2dd64aa56ab419377082606a9b1f03d338b43c1
Teun & Theun <t.vanderwijst@student.fontys.nl>
2015-12-10 14:50:58 +0100

    tostring methodes

--- ICT4RAILS - ASP.NET/ICT4RAILS - ASP.NET/Csharp/Sector.cs
+++ ICT4RAILS - ASP.NET/ICT4RAILS - ASP.NET/Csharp/Sector.cs
@@ -24,7 +24,63 @@ namespace ICT4RAILS___ASP.NET.Csharp
 
         public override string ToString()
         {
-            return base.ToString();
+            string idString;
+            if (ID == 0)
+            {
+                idString = "Onbekend";
+            }
+            else
+            {
+                idString = ID.ToString();
+            }
+
+            string tramString;
+            if (Tram == null)
+            {
+                tramString = "Onbekend";
+            }
+            else
+            {
+                tramString = Tram.ID.ToString();
+            }
+
+            string nummerString;
+            if (Nummer == 0)
+            {
+                nummerString = "Onbekend";
+            }
+            else
+            {
+                nummerString = Nummer.ToString();
+            }
+
+            string blokkadeString;
+            if (!Blokkade)
+            {
+                blokkadeString = "Nee";
+            }
+            else
+            {
+                blokkadeString = "Ja";
+            }
+
+            string beschikbaarString;
+            if (!Beschikbaar)
+            {
+                beschikbaarString = "Nee";
+            }
+            else
+            {
+                beschikbaarString = "Ja";
+            }
+
+            string info = "SectorID: " + idString
+                 + ", TramID: " + tramString
+                 + ", Nummer: " + nummerString
+                 + ", Beschikbaar: " + beschikbaarString
+                 + ", Geblokkeerd:" + blokkadeString;
+
+            return info;
         }
     }
 }
