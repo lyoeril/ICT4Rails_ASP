236d0e69dced48be25f95a5ee3d72d0f3ecdfc58
Vink,Yoeri Y.R.M <y.vink@student.fontys.nl>
2016-01-07 18:23:36 +0100

    troep weg!

--- ICT4RAILS - ASP.NET/ICT4RAILS - ASP.NET/Csharp/Administratie.cs
+++ ICT4RAILS - ASP.NET/ICT4RAILS - ASP.NET/Csharp/Administratie.cs
@@ -11,45 +11,21 @@ namespace ICT4RAILS___ASP.NET.Csharp
     public partial class Administratie
     {
         private Database data;
-
-        //public Remise Remise { get { return remise; } }
-        //public List<Functie> Functies { get { return functies; } }
-        //public List<Lijn> Lijnen { get { return lijnen; } }
-        //public List<Medewerker> Medewerkers { get { return medewerkers; } }
-        //public List<Recht> Rechten { get { return rechten; } }
-        //public List<Reservering> Reserveringen { get { return reserveringen; } }
-        //public List<Spoor> Sporen { get { return sporen; } }
-        //public List<Tram> Trams { get { return trams; } }
-        //public List<TramOnderhoud> Onderhoudsbeurten { get { return onderhoudsbeurten; } }
-        //public List<TramType> Typen { get { return typen; } }
         public List<Remise> Remises { get; private set; }
         public List<Functie> Functies { get; private set; }
         public List<Medewerker> Medewerkers { get; private set; }
         public List<TramOnderhoud> Onderhoudsbeurten { get; private set; }
         public List<TramType> Typen { get; private set; }
-//>>>>>>> 862ec7fb373141285339e892f09945f4d898ba4e
+
 
         public Administratie()
         {
-            //remise = new Remise();
-            //functies = new List<Functie>();
-            //lijnen = new List<Lijn>();
-            //medewerkers = new List<Medewerker>();
-            //rechten = new List<Recht>();
-            //reserveringen = new List<Reservering>();
-            //sporen = new List<Spoor>();
-            //trams = new List<Tram>();
-            //onderhoudsbeurten = new List<TramOnderhoud>();
-            //typen = new List<TramType>();
-
-            tableCells = new List<TableCell>();
-			
+            tableCells = new List<TableCell>();			
             data = new Database();
             Typen = data.GetAllTramtypes();
             Functies = data.GetAllFuncties();
             Medewerkers = data.GetAllMedewerkers();
-            Onderhoudsbeurten = new List<TramOnderhoud>();
-            
+            Onderhoudsbeurten = new List<TramOnderhoud>();        
             //Remises = data.GetAllRemises();
         }
     }
