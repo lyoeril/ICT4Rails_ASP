f4412821b0f8ac0a605981dfa0f3e1524d943023
Vink,Yoeri Y.R.M <y.vink@student.fontys.nl>
2016-01-12 18:46:02 +0100

    aanpassingen aan tram en de manier van het ophalen van de lijnen hiervan. een tram kan meerdere lijnen hebben

--- ICT4RAILS - ASP.NET/ICT4RAILS - ASP.NET/database/UpdateScripts.cs
+++ ICT4RAILS - ASP.NET/ICT4RAILS - ASP.NET/database/UpdateScripts.cs
@@ -53,27 +53,72 @@ namespace ICT4RAILS___ASP.NET.database
                 }
             }
         }
-
-        public List<Tram> GetAllOnderhoudTrams()
+        public void UpdateTram(Tram tram)
         {
-            List<Tram> tramlistTrams = new List<Tram>();
             using (OracleConnection connection = Connection)
             {
-                string query = "SELECT T.*, TL.\"Lijn_ID\" FROM TRAM T, TRAM_LIJN TL WHERE TL.\"Tram_ID\" = T.ID AND T.\"Defect\" = 1 OR T.\"Vervuild\" = 1";
-                using (OracleCommand command = new OracleCommand(query, connection))
+
+                string Update = "UPDATE TRAM SET \"Tramtype_ID\"=:TRAMTYPEID, \"Nummer\"=:NUMMER, \"Lengte\"=:LENGTE, \"Status\"=:STATUS,\"Vervuild\"=:VERVUILD, \"Defect\"=:DEFECT,\"ConducteurGeschikt\"=:CONDUCTEUR,\"Beschikbaar\"=:BESCHIKBAAR WHERE \"ID\"=:TRAMTYPEID";
+                using (OracleCommand command = new OracleCommand(Update, connection))
                 {
-                    List<TramType> tramtypes = GetAllTramtypes();
-                    List<Lijn> lijnen = GetAllLijnen();
-                    using (OracleDataReader reader = command.ExecuteReader())
+                    command.Parameters.Add(new OracleParameter("TRAMTYPEID", tram.TramType.ID));
+                    command.Parameters.Add(new OracleParameter("NUMMER", tram.Nummer));
+                    command.Parameters.Add(new OracleParameter("LENGTE", tram.Lengte));
+                    command.Parameters.Add(new OracleParameter("STATUS", tram.Status));
+                    int vervuild = 0;
+                    if (tram.Vervuild)
                     {
-                        while (reader.Read())
-                        {
-                            tramlistTrams.Add(CreateTramFromReader(reader, tramtypes, lijnen));
-                        }
+                        vervuild = 1;
                     }
+                    command.Parameters.Add(new OracleParameter("VERVUILD", vervuild));
+                    int defect = 0;
+                    if (tram.Defect)
+                    {
+                        defect = 1;
+                    }
+                    command.Parameters.Add(new OracleParameter("DEFECT", defect));
+                    int conducteurgeschikt = 0;
+                    if (tram.ConducteurGeschikt)
+                    {
+                        conducteurgeschikt = 1;
+                    }
+                    command.Parameters.Add(new OracleParameter("CONDUCTEUR", conducteurgeschikt));
+                    int beschikbaar = 0;
+                    if (tram.Beschikbaar)
+                    {
+                        beschikbaar = 1;
+                    }
+                    command.Parameters.Add(new OracleParameter("BESCHIKBAAR", beschikbaar));
+                    command.ExecuteNonQuery();
                 }
             }
-            return tramlistTrams;
+        }
+
+        public void UpdateSector(Sector sector)
+        {
+            using (OracleConnection connection = Connection)
+            {
+
+                string Update = "UPDATE SECTOR SET \"Tram_ID\" =:TRAMID,\"Beschikbaar\" =:BESCHIKBAAR,\"Blokkade\"=:BLOKKADE WHERE ID =:SECTORID";
+                using (OracleCommand command = new OracleCommand(Update, connection))
+                {
+                    command.Parameters.Add(new OracleParameter("SECTORID", sector.ID));
+                    command.Parameters.Add(new OracleParameter("TRAMID", sector.Tram.ID));
+                    int beschikbaar = 0;
+                    if (sector.Beschikbaar)
+                    {
+                        beschikbaar = 1;
+                    }
+                    int blokkade = 0;
+                    if (sector.Blokkade)
+                    {
+                        beschikbaar = 1;
+                    }
+                    command.Parameters.Add(new OracleParameter("BESCHIKBAAR", beschikbaar));
+                    command.Parameters.Add(new OracleParameter("BLOKKADE", blokkade));
+                    command.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
